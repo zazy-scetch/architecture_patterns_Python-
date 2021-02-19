40ce3e109f498cfe62bf1f74180ca0f2792d2845
Alexander <melkiy7dc@mail.ru>
2021-02-19 15:52:37 +0300

    Les_3

--- framework/templates.py
+++ framework/templates.py
@@ -1,24 +1,11 @@
-from jinja2 import Template
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 import os
 
 
 def render_(template_name, folder='templates', **kwargs):
-    """
-    :param template_name: имя шаблона
-    :param folder: папка в которой ищем шаблон
-    :param kwargs: параметры
-    :return:
-    """
-    file_path = os.path.join(folder, template_name)
-    # Открываем шаблон по имени
-    with open(file_path, encoding='utf-8') as f:
-        # Читаем
-        template = Template(f.read())
-    # рендерим шаблон с параметрами
-    return template.render(**kwargs)
 
-
-if __name__ == '__main__':
-    # Пример использования
-    output_test = render_('index.html', color_name='color')
-    print(output_test)
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    tmpl = env.get_template(template_name)
+    return tmpl.render(**kwargs)
