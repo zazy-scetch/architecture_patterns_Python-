8740e5d20f9dc5ee3756948e27b91bef4e7ec1d0
Zezin Aleksandr <57712823+zazy-scetch@users.noreply.github.com>
2021-02-26 15:01:26 +0300

    Les_1 (#1)

---
+++ wavy/templates.py
@@ -0,0 +1,24 @@
+from jinja2 import Template
+import os
+
+
+def render_(template_name, folder='templates', **kwargs):
+    """
+    :param template_name: имя шаблона
+    :param folder: папка в которой ищем шаблон
+    :param kwargs: параметры
+    :return:
+    """
+    file_path = os.path.join(folder, template_name)
+    # Открываем шаблон по имени
+    with open(file_path, encoding='utf-8') as f:
+        # Читаем
+        template = Template(f.read())
+    # рендерим шаблон с параметрами
+    return template.render(**kwargs)
+
+
+if __name__ == '__main__':
+    # Пример использования
+    output_test = render_('index.html', color_name='color')
+    print(output_test)
